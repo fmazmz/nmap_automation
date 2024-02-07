3649fc42223f9c7048fab5d436fbe0c673f29c06
Firas Moussa <fm222us@student.lnu.se>
2024-02-07 02:44:59 +0100

    Add input variables

--- nmap-automator.py
+++ nmap-automator.py
@@ -17,7 +17,15 @@ def print_intro():
 
 # Main Function
 def main():
+
+    # Print intro
     print(print_intro())
+
+    # Inputs (Modify this to your liking)
+    ip = "192.168.1.1/24"
+    script = "vuln"  # Leave empty for no scripts
+    ports = "445,80"  # Leave empty for ports 1-1000 / "-p-" for all ports
+    scan_type = "-sV -sT"  # Leave empty for SYN scan
 
 if __name__ == "__main__":
     main()
